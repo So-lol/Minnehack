f811d698cbb72f3e7612e024d28b645ec9450a8c
DaanishHindustaniTR <daanishhindustani@yahoo.com>
2026-02-14 17:20:33 -0600

    updated title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Campus Marketplace" }} />
+      <Stack.Screen name="index" options={{ title: "Gopher Exchange" }} />
       <Stack.Screen name="item/[id]" options={{ title: "Details" }} />
     </Stack>
   );
